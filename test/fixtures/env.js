66db75a939eec36aaee0ea2158cc11abebb2d5b2
Jérémie Astori <jeremie@astori.fr>
2017-11-22 16:55:09 -0500

    Switch default home location to `~/.thelounge`

--- test/fixtures/env.js
+++ test/fixtures/env.js
@@ -2,5 +2,5 @@
 
 global.log = require("../../src/log.js");
 
-var home = require("path").join(__dirname, ".lounge");
+var home = require("path").join(__dirname, ".thelounge");
 require("../../src/helper").setHome(home);
