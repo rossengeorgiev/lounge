b45e0c40d689ec4a6984f3c76812b32e7a3ba307
Jérémie Astori <jeremie@astori.fr>
2017-12-09 03:11:28 -0500

    Remove existing options of `thelounge start`, rather arbitrary and limited

--- src/command-line/start.js
+++ src/command-line/start.js
@@ -10,29 +10,12 @@ const Utils = require("./utils");
 
 program
 	.command("start")
-	.option("-H, --host <ip>", "set the IP address or hostname for the web server to listen on")
-	.option("-P, --port <port>", "set the port to listen on")
-	.option("-B, --bind <ip>", "set the local IP to bind to for outgoing connections")
-	.option("    --public", "start in public mode")
-	.option("    --private", "start in private mode")
 	.description("Start the server")
 	.on("--help", Utils.extraHelp)
-	.action(function(options) {
+	.action(function() {
 		initalizeConfig();
 
 		const server = require("../server");
-
-		var mode = Helper.config.public;
-		if (options.public) {
-			mode = true;
-		} else if (options.private) {
-			mode = false;
-		}
-
-		Helper.config.host = options.host || Helper.config.host;
-		Helper.config.port = options.port || Helper.config.port;
-		Helper.config.bind = options.bind || Helper.config.bind;
-		Helper.config.public = mode;
 
 		server();
 	});
